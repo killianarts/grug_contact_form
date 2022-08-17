b1d14a16f52ec1ddafe219595b529aaeb771189c
Micah Killian <micah@killianarts.online>
2022-08-17 14:29:00 +0900

    Added confirmation_view

--- contact/core/views.py
+++ contact/core/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
 from django.template import loader
 from .forms import ContactForm
 
@@ -6,6 +7,16 @@ from .forms import ContactForm
 def contact_page_view(request):
     template = loader.get_template('contact_page.html')
     form = ContactForm()
+    context = {
+        'form': form
+    }
+    return HttpResponse(template.render(context, request))
+
+
+@require_POST
+def confirmation_view(request):
+    template = loader.get_template('confirmation_page.html')
+    form = ContactForm(request.POST)
     context = {
         'form': form
     }
