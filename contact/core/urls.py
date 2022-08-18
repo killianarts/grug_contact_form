aaa4dc05192f91cd61e32cc1d5acd35ae1758997
Micah Killian <micah@killianarts.online>
2022-08-18 14:29:27 +0900

    Renamed submit to success.

--- contact/core/urls.py
+++ contact/core/urls.py
@@ -5,6 +5,5 @@ from .views import ContactView, ContactDataPreview, ContactSuccessView
 urlpatterns = [
     path('', ContactView.as_view(), name='contact_page'),
     path('confirm/', ContactDataPreview(ContactForm)),
-    # path('confirm', views.confirmation_page_view, name='confirmation_page'),
-    path('submit/', ContactSuccessView.as_view(), name='success_page')
+    path('success/', ContactSuccessView.as_view(), name='success_page')
 ]
