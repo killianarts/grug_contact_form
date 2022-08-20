fbbace1abcad8c28896328cfcfc9942467a8f0c2
Micah Killian <mijokijo@gmail.com>
2022-08-20 13:54:38 +0900

    Cleaned up function-based views

--- contact/core/views.py
+++ contact/core/views.py
@@ -4,7 +4,7 @@ from .forms import ContactForm
 from django.views.decorators.http import require_POST
 
 
-def contact_page_view(request: HtmxHttpRequest) -> HttpResponse:
+def contact_page_view(request: HttpRequest) -> HttpResponse:
     template = 'contact_page.html'
     base_template = 'contact_form_partial.html'
     form = ContactForm()
@@ -14,11 +14,16 @@ def contact_page_view(request: HtmxHttpRequest) -> HttpResponse:
     }
     return render(request, template, context)
 
+
 @require_POST
-def confirmation_view(request: HtmxHttpRequest) -> HttpResponse:
+def confirmation_view(request: HttpRequest) -> HttpResponse:
     template = 'confirmation_page.html'
     base_template = 'contact_form_partial.html'
     form = ContactForm(request.POST)
+    context = {
+        'form': form,
+        'base_template': base_template,
+    }
     if form.is_valid():
         context = {
             'form': form,
@@ -26,17 +31,16 @@ def confirmation_view(request: HtmxHttpRequest) -> HttpResponse:
         }
         return render(request, template, context)
 
+
 @require_POST
-def send_view(request: HtmxHttpRequest) -> HttpResponse:
+def send_view(request: HttpRequest) -> HttpResponse:
     template = 'success_page.html'
     base_template = 'contact_form_partial.html'
     form = ContactForm(request.POST)
-    if form.is_valid():
-        form.send()
-    else:
-        return HttpResponse("Whoops")
     context = {
         'form': form,
         'base_template': base_template,
     }
+    if form.is_valid():
+        form.send()
     return render(request, template, context)
