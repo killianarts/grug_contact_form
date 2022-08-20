4cac3042f575cd842c0bc176b321ec8a7a24e6ef
Micah Killian <mijokijo@gmail.com>
2022-08-20 13:56:30 +0900

    Switched to class-based views in urls.py

--- contact/core/urls.py
+++ contact/core/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .forms import ContactForm
 from . import views
 
+# urlpatterns = [
+#     path('', views.contact_page_view, name='contact_page'),
+#     path('confirm/', views.confirmation_view),
+#     path('success/', views.send_view)
+# ]
+
 urlpatterns = [
-    path('', views.contact_page_view, name='contact_page'),
-    path('confirm/', views.confirmation_view),
-    path('success/', views.send_view)
+    path('', views.ContactPageView.as_view(), name='contact_page'),
+    path('confirm/', views.ConfirmationView.as_view()),
+    path('success/', views.SuccessView.as_view())
 ]
