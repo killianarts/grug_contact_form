767f7b23d8801253ce718f7cf8b3b94b21901d26
Micah Killian <micah@killianarts.online>
2022-08-20 09:26:34 +0900

    Updated urls.py
    Converted to function based views.

--- contact/core/urls.py
+++ contact/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .forms import ContactForm
-from .views import ContactView, ContactDataPreview, ContactSuccessView
+from . import views
 
 urlpatterns = [
-    path('', ContactView.as_view(), name='contact_page'),
-    path('confirm/', ContactDataPreview(ContactForm)),
-    path('success/', ContactSuccessView.as_view(), name='success_page')
+    path('', views.contact_page_view, name='contact_page'),
+    path('confirm/', views.confirmation_view),
+    path('success/', views.send_view)
 ]
