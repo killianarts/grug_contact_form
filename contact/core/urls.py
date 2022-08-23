aa150aae5684aed9cce65edd30069f70653ecd34
Micah Killian <micah@killianarts.online>
2022-08-23 14:33:25 +0900

    Added send/ path to urls

--- contact/core/urls.py
+++ contact/core/urls.py
@@ -4,11 +4,12 @@ from . import views
 # urlpatterns = [
 #     path('', views.contact_page_view, name='contact_page'),
 #     path('confirm/', views.confirmation_view),
-#     path('success/', views.send_view)
+#     path('send/', views.send_view)
 # ]
 
 urlpatterns = [
     path('', views.ContactPageView.as_view(), name='contact_page'),
     path('confirm/', views.ConfirmationView.as_view()),
+    path('send/', views.ConfirmationView.as_view(submit=True)),
     path('success/', views.SuccessView.as_view())
 ]
